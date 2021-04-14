b230353aa21a124f3692410adc44bded382d3b2a
Sasha9805 <pimenovs33@gmail.com>
2021-04-14 17:06:08 +0300

    Добавлен роутинг и компоненты, отвечающие за отрисовку списка книг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
 
 import './app.css';
-import { withBookstoreService } from '../hoc';
+import { HomePage, CartPage } from "../pages";
 
-const App = ({ bookstoreService }) => {
-  console.log(bookstoreService.getBooks());
+const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Switch>
+      <Route
+        path="/"
+        component={HomePage}
+        exact />
+      <Route
+        path="/cart"
+        component={CartPage} />
+    </Switch>
   );
 };
 
-export default withBookstoreService()(App);
+export default App;

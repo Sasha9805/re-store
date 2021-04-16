c7e7ee3f3d39844284752d66bbeada04aefc729f
Sasha9805 <pimenovs33@gmail.com>
2021-04-16 17:19:34 +0300

    Добавлен компонент-контейнер для BookList

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -9,7 +9,23 @@ import './book-list.css';
 import Spinner from "../spinner";
 import ErrorIndicator from "../error-indicator";
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+  return (
+    <ul className="book-list">
+      {
+        books.map(book => {
+          return (
+            <li key={book.id}>
+              <BookListItem book={book} />
+            </li>
+          );
+        })
+      }
+    </ul>
+  );
+};
+
+class BookListContainer extends Component {
 
   componentDidMount() {
     this.props.fetchBooks();
@@ -26,19 +42,8 @@ class BookList extends Component {
       return <ErrorIndicator />;
     }
 
-    return (
-      <ul className="book-list">
-        {
-          books.map(book => {
-            return (
-              <li key={book.id}>
-                <BookListItem book={book} />
-              </li>
-            );
-          })
-        }
-      </ul>
-    );
+    return <BookList books={books} />;
+
   }
 
 }
@@ -56,4 +61,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
